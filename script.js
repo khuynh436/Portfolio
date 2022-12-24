0445bdca35b1f3881cdcb33b2f5f193f07227d91
khuynh436 <khuynh436@gmail.com>
2022-12-23 17:44:47 -0800

    Rename and add javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var greeting = document.createElement('p');
+    greeting.innerHTML = 'Hello and welcome to my portfolio!';
+    document.getElementById('about').appendChild(greeting);
+  }
+  
